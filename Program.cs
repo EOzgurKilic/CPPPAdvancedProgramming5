95082f0ea1c63237f56fdbabed77405409986c86
orkhaner <orkhanrrahimli@gmail.com>
2025-01-13 16:12:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CPPPAdvancedProgramming5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Implicit&Explicit Conversion Overloading
+        //In short, apart from utilizing from polymorphism or explicit&implicit conversions; there is no way to assign a variable to a different type reference point.
+    }
+}
