ce174927dd882a913e2acb068f8837bf9cba54f5
Özgür Kılıç <ozgurkilic147@gmail.com>
2025-11-13 23:04:40 +0100

    Tutorial texts revised.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //Implicit&Explicit Conversion Overloading
-        //In short, apart from utilizing from polymorphism or explicit&implicit conversions; there is no way to assign a variable to a different type reference point.
+        //In short, apart from utilizing from polymorphism or explicit&implicit conversions; there is no way to assign an instance to a different type reference point.
         /*A objA1 = new A(){ANo = 10};
         A objA2 = new A(){ANo = 15};
         B objB1 = new B(){BNo = 20};
